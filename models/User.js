04daa05b53d8281b5624b13e8f1acf51d6c3d0ce
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-06 17:18:22 -0500

    Remove email

--- models/User.js
+++ models/User.js
@@ -6,15 +6,7 @@ const UserSchema = new Schema ({
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true
-    },
     password: {
-        type: String,
-        required: true
-    },
-    password2: {
         type: String,
         required: true
     },
