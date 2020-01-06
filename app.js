84e38b21fa61ed81e90566eb37bb37c15a4ab7ee
Derek Shue <dshue@berkeley.edu>
2020-01-06 16:05:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.set('useUnifiedTopology', true);
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
