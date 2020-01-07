7730581c3a81089fcd8e4ae907ea2f70c4203283
deb <lee025@gmail.com>
2020-01-07 18:26:31 -0500

    UserAuth working!!!!!

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, withRouter } from 'react-router-dom';
+import './navbar.css'
 
 class NavBar extends React.Component {
 
     constructor(props){
         super(props)
 
-      this.renderSessionButton = this.renderSessionButton.bind(this);
+      this.logoutUser = this.logoutUser.bind(this);
       this.handleUpload = this.handleUpload.bind(this);
     }
   
     logoutUser(e) {
       e.preventDefault();
-      this.props.logout();
+      if (this.props.currentUser) {
+        this.props.logout()
+      } else {
+        this.props.history.push('/')
+      }
     }
 
     handleUpload(e){
@@ -29,28 +34,34 @@ class NavBar extends React.Component {
         )
       } else {
         return (
-          <div>
-            <Link to='/login'>Sign In</Link>
+          <div className='signin-button-container'>
+            <i className="fas fa-user-circle"></i>
+            <Link to='/api/users/login' className='signin-button'>Sign In</Link>
           </div>
         )
       }
     }
 
     render() {
+      console.log(this.props)
       return (
         <div className="main">
+          <i className="fas fa-bars"></i>
           <div className="left">
-            <i className="fab fa-youtube-square"></i>
+            <i className="fab fa-youtube"></i>
+            YouTube
           </div>
 
           <div className="center">
             {/* Search bar will be here */}
-            <input type='submit' placeholder='Search' />
+            <input type='text' className='searchbar' placeholder='Search' />
+            <i className="fas fa-search"></i>
           </div>
 
           <div className="right">
             <i className="fas fa-video" onClick={this.handleUpload}></i>
-            {this.renderSessionButton}
+            {this.renderSessionButton()}
+            {/* <button onClick={this.renderSessionButton}>Sign Out</button> */}
           </div>
         </div>
       );
@@ -59,4 +70,4 @@ class NavBar extends React.Component {
 }
 
 
-export default NavBar;
+export default withRouter(NavBar);
--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,23 +1,21 @@
 import { connect } from 'react-redux';
 import NavBar from './navbar';
 
-import { logout, login } from '../../actions/session_actions'; // verify session_actions
+import { logout } from '../../actions/session_actions'; // verify session_actions
 
 const mapStateToProps = (state, ownProps) => {
-    
-    // let currentUser = state.entities.users[state.session.id]
-    let currentUser = state.session.id
+    // console.log(state)
+    let currentUser = state.session.user.id
+      return {
+          currentUser,
+          loggedIn: state.session.isAuthenticated
+      }
 
-    return {
-        currentUser,
-        loggedIn: state.session.isAuthenticated
-    }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     logout: () => dispatch(logout()),
-    login: user => dispatch(login(user)),
   }
 }
 
