486ecbe402ce0e7e77528cfbdcecab9d536c8a5d
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-08 17:08:34 -0500

    Fixed videoDisplay video size ..sry about master push :'(

--- frontend/src/components/videos/video_index_item_container.js
+++ frontend/src/components/videos/video_index_item_container.js
@@ -3,10 +3,6 @@ import VideoIndexItem from './video_index_item';
 import { fetchVideo, deleteVideo } from '../../actions/video_actions';
 
 const mapStateToProps = (state, { video }) => {
-<<<<<<< HEAD
-=======
-    // console.log(state)
->>>>>>> 754a956d73a037893d331bc44003947d05b15246
     return {
         video
     }
