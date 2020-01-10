4350b422c576f6fc8f659694bb44a5ee213c4692
Sadman Anwar <sadmanowar@gmail.com>
2020-01-09 20:31:00 -0500

    anything

--- frontend/src/reducers/ui_reducer.js
+++ frontend/src/reducers/ui_reducer.js
@@ -1,13 +1,15 @@
-import { UPDATE_SEARDH_FIELD, UPDATE_SEARCH_FIELD } from '../actions/ui_actions';
+import { UPDATE_SEARCH_FIELD } from '../actions/ui_actions';
 import { merge } from 'lodash';
 
 const uiReducer = (state={},action) => {
    
     Object.freeze(state);
-
+    const newState = merge({},state)
   switch(action.type){
     case UPDATE_SEARCH_FIELD: 
       return merge(newState, {search: action.search})
+    default: 
+       return state
   }
 }
 
