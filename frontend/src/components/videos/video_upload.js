1c6f3066aa9baa874a5586693834f4b699aa1800
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-10 18:54:05 -0500

    Upload works

--- frontend/src/components/videos/video_upload.js
+++ frontend/src/components/videos/video_upload.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../stylesheets/video_upload.css';
 
 class VideoUpload extends React.Component {
     constructor(props) {
@@ -11,45 +12,67 @@ class VideoUpload extends React.Component {
         }
 
         this.update = this.update.bind(this);
+        this.upload = this.upload.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const { title, user_id, video } = this.state;
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('user_id', user_id);
+        formData.append('video', video);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        };
+
+        this.props.createVideo(formData, config);
+    }
+
+    upload(e) {
+        this.setState({ 'video': e.target.files[0]});
     }
 
     update(field) {
         return e => this.setState({ [field]: e.currentTarget.value})
     };
 
-    handleSubmit(e) {
-        e.preventDefault();
-        let newVideo = {
-            title: this.state.title,
-            user_id: this.state.user_id,
-            video: this.state.video
-        };
-
-        this.props.createVideo(newVideo);
-    }
-
 
     render() {
         return (
-            <div className="video-upload-form">
-                <form onSubmit={this.handleSubmit}>
-                    <label className="video-title">Title
-                        <input
-                            type="text"
-                            value={this.state.title}
-                            onChange={this.update('title')}
-                            />
-                    </label>
-                    <label className="video-upload">Upload!
-                        <input
-                            type="file"
-                            value={this.state.video}
-                            onChange={this.update('video')}
-                            />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
+            <div className="video-upload-container-background">
+                <div className="top-margin"></div>
+                <div className="video-upload-container">
+                    <div className="v-u-header">
+                        <h1 className="upload-video">Upload video</h1>
+                        <div>&times;</div>
+                    </div>
+                    <div className="v-u-form-container">
+                        <form onSubmit={this.handleSubmit} className="v-u-form">
+                            <label className="video-title">Title
+                                <input
+                                    type="text"
+                                    value={this.state.title}
+                                    onChange={this.update('title')}
+                                    />
+                            </label>
+                            <br></br>
+                            <label className="video-upload">Upload!
+                                <input
+                                    type="file"
+                                    name="video"
+                                    onChange={this.upload}
+                                    />
+                            </label>
+                            <br></br>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -8,7 +8,6 @@ const UsersReducer = (state = {}, action) => {
     switch(action.type){
         
          case RECEIVE_CURRENT_USER: 
-            debugger;
             return Object.assign({}, state, { [action.currentUser.id]: action.user})
          case RECEIVE_USER:
             return Object.assign({}, state, {[action.user.id]:action.user})
