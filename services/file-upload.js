0a2f0c5d51bd41c5ae394282414004e118d03d92
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-13 18:52:58 -0500

    Only dev bucket

--- services/file-upload.js
+++ services/file-upload.js
@@ -23,7 +23,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'metube-aa-pro',
+        bucket: 'metube-aa-dev',
         acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, {fieldName: 'TESTING META DATA!' });
