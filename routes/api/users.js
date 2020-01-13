395c40d6de8f45824e407926c06195b4337012bb
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-13 13:20:28 -0500

    Debugging Heroku

--- routes/api/users.js
+++ routes/api/users.js
@@ -40,6 +40,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+    console.log("HIT THIS");
     const { errors, isValid } = validateLoginInput(req.body);
     if (!isValid) {
         return res.status(400).json(errors);
