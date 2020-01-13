89101ba29651502b6b2a30a5a6036a9a49e2d40d
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-13 15:57:54 -0500

    Debugging heroku

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.get( '/', (req, res) => res.send('Hello World'))
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 console.log("THIS IS PORT", port);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
