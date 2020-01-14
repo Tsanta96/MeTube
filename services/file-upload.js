a9c7b714d29584c5cdb8b7164d8b9cfaf6610e5e
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-01-14 11:15:07 -0500

    Test prd and dev buckets

--- services/file-upload.js
+++ services/file-upload.js
@@ -4,13 +4,15 @@ const multerS3 = require('multer-s3');
 const keys = require('../config/keys');
 
 
-// console.log("PROCESS ENV", process.env);
-// let bucketName;
-// if (process.env.NODE_ENV === 'production') {
-//     bucketName = 'metube-aa-pro';
-// } else {
-//     bucketName = 'metube-aa-dev';
-// }
+console.log("PROCESS ENV", process.env);
+let bucketName;
+if (process.env.NODE_ENV === 'production') {
+    bucketName = 'metube-aa-pro';
+    console.log("Bucketname: " + bucketName);
+} else {
+    bucketName = 'metube-aa-dev';
+    console.log("Bucketname: " + bucketName);
+}
 
 aws.config.update({
     accessKeyId: keys.iam_access_id,
@@ -23,7 +25,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'metube-aa-dev',
+        bucket: bucketName,
         acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, {fieldName: 'TESTING META DATA!' });
